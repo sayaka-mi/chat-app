ef8a1e96e05c348af71360aabd9f41c1bc075d07
sayaka-mi <sayakakarai@gmail.com>
2025-04-22 10:45:03 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,9 @@ class Message < ApplicationRecord
   # 1つのメッセージは、1つのチャットルームに存在
   belongs_to :user
   # 1つのメッセージは、1人のユーザーから送信
+  has_one_attached :image
+  # messagesテーブルにカラムを追加する必要はない
+
 
   validates :content, presence: true
   # 空の場合はDBに保存しない
